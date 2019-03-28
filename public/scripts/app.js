f98bce074fc89449e20849be4332378385a6ee28
Gurp <kooner@gmail.com>
2019-03-28 02:03:09 +0000

    created form for polls and able to add and remove options

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,49 @@
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+  let options_div = $('.options');
+  let optionCount = 2;
+
+  options_div.append(createOption(0));
+  $poll = createOption(1);
+  let add_btn = $('<button><i class="fas fa-plus"></i></button>');
+  add_btn.on('click', function(e) {
+    e.preventDefault();
+    let $newOption = createOption(optionCount);
+    $newOption.append(this);
+    options_div.append($newOption);
+    optionCount++;
+  });
+  $poll.append(add_btn);
+  options_div.append($poll);
+
+  function createOption (num) {
+    let $poll = $('<div class="form-group"></div>');
+    let $label = $(`<label for="option">${num + 1}:</label>`);
+    let $input = $('<input type="text" class="form-control" placeholder="Enter your option">');
+    let $del_btn = $('<button><i class="fas fa-times"></i></button>');
+    $del_btn.on('click', function(e){
+      e.preventDefault();
+      optionCount--;
+      $poll.remove();
+      let children = $('.options').children()//.next();
+      let counter = optionCount - 1;
+      children.each(function(){
+        $(this).find('label').text(optionCount - counter);
+        counter--;
+      })
+      // while (nextSibling.next()) {
+      //   nextSibling.find('label').text(optionCount);
+      //   nextSibling = nextSibling.next();
+      // }
+      
+    });
+    $poll.append($label);
+    $poll.append($input);
+    $poll.append($del_btn);
+    return $poll;
+  }
 });
+
+
+
+                            
+                        
