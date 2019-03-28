cae147a7928b7ab39e2a91a6d95aa4eac2f48094
Gurp <kooner@gmail.com>
2019-03-28 20:06:42 +0000

    Nested knex insert for choices

--- routes/polls.js
+++ routes/polls.js
@@ -2,21 +2,28 @@
 
 const express = require('express');
 const router  = express.Router();
+const crypto = require('crypto');
+
+function generateRandomString() {
+  return crypto.randomBytes(3).toString('hex');
+}
 
 module.exports = (knex) => {
 
   router.post("/new", (req, res) => {
     let data = req.body.data;
-    knex('polls').insert({creator_email: data[0], timestamp: new Date(), 
-      submission_url_id: 'rand0m', admin_url_id: 'rand0m'})
+    knex('polls').insert({creator_email: data[0], title: data[1], timestamp: new Date(), 
+      submission_url_id: generateRandomString(), admin_url_id: generateRandomString()})
       .returning('id')
       .then( function (id) {
-        knex('choices').insert({creator_email: data[0], timestamp: new Date(), 
-          poll_id: id[0], title: data[2]})
-          .returning('id')
-          .then( function (result) {
-              console.log(result)
-           })
+        for (let i = 2; i < data.size; i++) {
+          knex('choices').insert({creator_email: data[0], timestamp: new Date(), 
+            poll_id: id[0], title: data[2]})
+            .returning('id')
+            .then( function (result) {
+                console.log(result)
+             })
+        }
        })
   });
 
