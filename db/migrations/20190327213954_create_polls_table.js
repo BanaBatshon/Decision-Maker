714b8a3dda7194830f28ffbc56e634b5d988748a
Bana <banabatshon@hotmail.com>
2019-03-28 23:00:32 +0000

    updates the migrations

--- db/migrations/20190327213954_create_polls_table.js
+++ db/migrations/20190327213954_create_polls_table.js
@@ -1,9 +1,9 @@
-
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('polls', function (table) {
     table.increments();
     table.string('creator_email');
     table.date('timestamp');
+    table.string('title');
     table.string('submission_url_id');
     table.string('admin_url_id');
   });
