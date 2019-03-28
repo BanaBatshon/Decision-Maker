0d7ea590f8de03d32e294a19ad9520c199fdffb0
Bana <banabatshon@hotmail.com>
2019-03-28 20:14:43 +0000

    creats seed files for polls, choices and submissions

---
+++ db/seeds/01_choices.js
@@ -0,0 +1,18 @@
+//insert dummy data for choices
+exports.seed = function(knex, Promise) {
+  return knex('choices').del()
+  .then(function () {
+    return knex('polls').select('id');
+  })
+  .then(function(rows) {
+    return Promise.all([
+      knex('choices').insert({poll_id: rows[0].id, title: 'movie1', description: 'romcom'}),
+      knex('choices').insert({poll_id: rows[0].id, title: 'movie2', description: 'drama'}),
+      knex('choices').insert({poll_id: rows[0].id, title: 'movie3', description: 'horror'}),
+      knex('choices').insert({poll_id: rows[0].id, title: 'movie4', description: 'action'}),
+    ])
+  });
+};
+
+
+// knex('submission_choices').insert({}),
