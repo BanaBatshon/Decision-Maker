c6e383ddffe5653f6f41cb6ffac24eb6e50b2a1f
Gurp <kooner@gmail.com>
2019-03-28 17:09:59 +0000

    Front end for options working can add and delete inputs

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,15 +4,8 @@ $(() => {
 
   options_div.append(createOption(0));
   $poll = createOption(1);
-  let add_btn = $('<button><i class="fas fa-plus"></i></button>');
-  add_btn.on('click', function(e) {
-    e.preventDefault();
-    let $newOption = createOption(optionCount);
-    $newOption.append(this);
-    options_div.append($newOption);
-    optionCount++;
-  });
-  $poll.append(add_btn);
+  let $add_btn = createAddBtn();
+  $poll.append($add_btn);
   options_div.append($poll);
 
   function createOption (num) {
@@ -20,26 +13,41 @@ $(() => {
     let $label = $(`<label for="option">${num + 1}:</label>`);
     let $input = $('<input type="text" class="form-control" placeholder="Enter your option">');
     let $del_btn = $('<button><i class="fas fa-times"></i></button>');
-    $del_btn.on('click', function(e){
+    $del_btn.on('click', function(e) {
       e.preventDefault();
+      let addBtnRemoved = false;
+      if ($poll.find('label').text().replace(/\:/, '') === (optionCount).toString()) {
+        addBtnRemoved = true;
+      }
       optionCount--;
       $poll.remove();
-      let children = $('.options').children()//.next();
+      let children = $('.options').children();
       let counter = optionCount - 1;
-      children.each(function(){
-        $(this).find('label').text(optionCount - counter);
+      children.each(function() {
+        if (addBtnRemoved && counter === 0) {
+          let $add_btn = createAddBtn();
+          $(this).append($add_btn);
+        }
+        $(this).find('label').text(`${optionCount - counter}:`);
         counter--;
-      })
-      // while (nextSibling.next()) {
-      //   nextSibling.find('label').text(optionCount);
-      //   nextSibling = nextSibling.next();
-      // }
-      
+      });
     });
     $poll.append($label);
     $poll.append($input);
     $poll.append($del_btn);
     return $poll;
+  }
+
+  function createAddBtn() {
+    let $add_btn = $('<button><i class="fas fa-plus"></i></button>');
+    $add_btn.on('click', function(e) {
+      e.preventDefault();
+      let $newOption = createOption(optionCount);
+      $newOption.append(this);
+      options_div.append($newOption);
+      optionCount++;
+    });
+    return $add_btn;
   }
 });
 
