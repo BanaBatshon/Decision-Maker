8dbc939420f61ee2edabbd07d61799434817369b
Bana <banabatshon@hotmail.com>
2019-03-28 18:42:45 +0000

    Creates the bordaCount function which calculates the percentage points given an ordered ranking

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,20 @@
 $(() => {
   $.ajax({
     method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
+    url: "/api/polls"
+  }).done((polls) => {
+    for(poll of polls) {
       $("<div>").text(user.name).appendTo($("body"));
     }
   });;
 });
+
+const bordaCount = function(rankingArr) {
+  const points = [];
+  const percentage = 100 / rankingArr.length;
+
+  for (rank of rankingArr) {
+    points.push((rankingArr.length - rankingArr[rank + 1]) * percentage)
+  }
+  return points;
+}
