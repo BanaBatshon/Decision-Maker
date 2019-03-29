0f74d998e6f52dc83d56ab07bf27796c5959e4bf
Bana <banabatshon@hotmail.com>
2019-03-29 23:20:18 +0000

    joins the two tables to display the title on the chart instead of the choice_id

--- db/seeds/03_submissions_choices.js
+++ db/seeds/03_submissions_choices.js
@@ -4,42 +4,33 @@ exports.seed = function(knex, Promise) {
   .then(function () {
     return Promise.all([
       knex('submissions').select('id'),
-      knex('choices').select('id')
+      knex('choices').select('id','title')
     ])
   })
   .then(function(rows) {
     const [submission_id, choice_id] = rows;
-
     const submission_choices = [];
     submission_id.forEach(submission => {
+      const options = [];
+      while(options.length < choice_id.length) {
+        let rank = (Math.floor(Math.random() * choice_id.length) + 1)
+        if (options.indexOf(rank) === -1) {
+          options.push( rank );
+        }
+      }
+      let i = 0;
       choice_id.forEach(choice => {
         submission_choices.push({
           submission_id: submission.id,
           choice_id: choice.id,
-          rank: Math.floor(Math.random() * choice_id.length)
+          rank: options[i]
         });
+        i ++;
       });
     });
     
-    
-
     return knex('submission_choices').insert(submission_choices);
 
-
-    // return Promise.all([
-    //   knex('submission_choices').insert({submission_id: rows[0][0].id, choice_id: rows[1][0].id, rank:0}),
-    //   knex('submission_choices').insert({submission_id: rows[0][0].id, choice_id: rows[1][1].id, rank:1}),
-    //   knex('submission_choices').insert({submission_id: rows[0][0].id, choice_id: rows[1][2].id, rank:2}),
-    //   knex('submission_choices').insert({submission_id: rows[0][0].id, choice_id: rows[1][3].id, rank:3}),
-    //   knex('submission_choices').insert({submission_id: rows[0][1].id, choice_id: rows[1][0].id, rank:0}),
-    //   knex('submission_choices').insert({submission_id: rows[0][1].id, choice_id: rows[1][1].id, rank:1}),
-    //   knex('submission_choices').insert({submission_id: rows[0][1].id, choice_id: rows[1][2].id, rank:2}),
-    //   knex('submission_choices').insert({submission_id: rows[0][1].id, choice_id: rows[1][3].id, rank:3}),
-    //   knex('submission_choices').insert({submission_id: rows[0][2].id, choice_id: rows[1][0].id, rank:0}),
-    //   knex('submission_choices').insert({submission_id: rows[0][2].id, choice_id: rows[1][1].id, rank:1}),
-    //   knex('submission_choices').insert({submission_id: rows[0][2].id, choice_id: rows[1][2].id, rank:2}),
-    //   knex('submission_choices').insert({submission_id: rows[0][2].id, choice_id: rows[1][3].id, rank:3})
-    // ])
   })
   .then(function() {
     return Promise.all(
