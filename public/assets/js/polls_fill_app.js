e6d3347c5959ff6bb47a6fb7b1797fc6e77f4a4c
Andrew Li <mail@andrewli.ca>
2019-03-31 10:18:13 +0000

    Refactor poll submission page, add error and post submission page

--- public/assets/js/polls_fill_app.js
+++ public/assets/js/polls_fill_app.js
@@ -1,19 +1,131 @@
 $(() => {
   let search = location.search.substring(1).slice(4);
-  let $ul = $('#respond-to-poll-options');
-  function createInput(choice, index) {
-    let $li;
-    if(choice.description.length === 0) {
-      $li = $(`<li class="list-group-item"><div class="sortable-number">${index + 1}</div><span class="choice-title">${choice.title}</span> <span class="choice-desc"></span></li>`);
+  getPollOptionsAndRender(search);
+});
+
+/**
+ * 
+ * @param {string} search 
+ */
+function getPollOptionsAndRender(search) {
+  $.ajax({
+    url: '/polls/' + search,
+    method: 'GET',
+    success: function (results) {
+      console.log(results);
+      renderPollSelectionForm(results);
+      renderPollChoices(results.choices);
+
+    },
+    error: function (err) {
+      console.log(err);
+      renderErrorPage();
     }
-    else {
-      $li = $(`<li class="list-group-item"><div class="sortable-number">${index + 1}</div><span class="choice-title">${choice.title}</span> <span class="choice-desc">(${choice.description})</span></li>`);
-    }
-    
-    $li.attr('id', choice.id);
-    $li.attr('poll_id', choice.poll_id);
-    return $li;
-  }
+  });
+}
+/**
+ * Render error page when link is invalid
+ */
+function renderErrorPage() {
+  var $choosePollOptionsSection = $('#choose-poll-options > div')
+  $choosePollOptionsSection.css('display','flex');
+  var $errorDiv = $('<div class="error-message row d-flex flex-column justify-content-center">');
+  var $h1 = $(`<h1>Page not found</h1>`);
+  var $p = $('<p>Sorry, the requested url was not found on the server.</p>')
+  var $imgDiv = $('<div class="row d-flex flex-column justify-content-center">');
+  var $img = $('<img src="/assets/img/404_dude.png">')
+
+  $errorDiv.append($h1);
+  $errorDiv.append($p);
+
+  $imgDiv.append($img);
+  $choosePollOptionsSection.append($errorDiv);
+  $choosePollOptionsSection.append($imgDiv);
+}
+
+/**
+ * Render page after poll has been submitted successfully
+ */
+function renderPostSubmissionPage() {
+  var $choosePollOptionsSection = $('#choose-poll-options > div');
+  $choosePollOptionsSection.empty();
+  $choosePollOptionsSection.css('display','flex');
+  var $errorDiv = $('<div class="error-message row d-flex flex-column justify-content-center">');
+  var $h1 = $(`<h1>Poll submitted!</h1>`);
+  var $p = $('<p>Thanks for participating, the poll creator will be notified.</p>')
+  var $imgDiv = $('<div class="row d-flex flex-column justify-content-center">');
+  var $img = $('<img src="/assets/img/happy_dude.png">')
+
+  $errorDiv.append($h1);
+  $errorDiv.append($p);
+
+  $imgDiv.append($img);
+  $choosePollOptionsSection.append($errorDiv);
+  $choosePollOptionsSection.append($imgDiv);
+}
+
+/**
+ * Render poll section form layout
+ * @param {object} poll 
+ */
+function renderPollSelectionForm(poll) {
+  var $choosePollOptionsSection = $('#choose-poll-options > div')
+  var $h1 = $(`<h1 class="heading-page respond-to-poll-title">${poll.title}</h1>`);
+  $choosePollOptionsSection.append($h1);
+
+  var $formDiv = $('<div class="form-group row d-flex justify-content-center">');
+  var $form = $('<form class="col-6">');
+  var $nameLabel = $('<label for="name" class="col-form-label">Please enter your name:</label>');
+  var $nameInput = $('<input class="form-control form-control-lg" id="name" type="text" aria-describedby="emailHelp" placeholder="eg. John Smith">');
+  var $h4 = $('<h4 class="heading-form">Rank the following options:</h4>');
+  var $ul = $('<ul id="respond-to-poll-options" class="list-group options">');
+  var $buttonDiv = $('<div class="row d-flex justify-content-around">');
+  var $button = $('<button type="submit" class="btn btn-primary btn-lg btn-custom">Submit</button>');
+
+  $button.on('click', function (e) {
+    e.preventDefault();
+    let choiceArr = [];
+    let rank = $ul.children().length;
+    let poll_id;
+    $ul.children().each(function () {
+      let choiceObj = {};
+      poll_id = $(this).attr('poll_id');
+      choiceObj['choice_id'] = $(this).attr('id');
+      choiceObj['rank'] = rank;
+      choiceArr.push(choiceObj);
+      rank--;
+    });
+    let search = location.search.substring(1).slice(4);
+    $.post('/polls/' + search, { 'name': $('input').val(), 'poll_id': poll_id, 'choiceArr': choiceArr })
+      .done(function (res) {
+
+        renderPostSubmissionPage();
+      });
+  });
+
+  $form.append($nameLabel);
+  $form.append($nameInput);
+  $form.append($h4);
+  $form.append($ul);
+  $form.append($buttonDiv);
+  $form.append($button);
+
+  $formDiv.append($form);
+  $choosePollOptionsSection.append($formDiv);
+}
+
+/**
+ * Render poll options
+ * @param {*} choice 
+ * @param {*} index 
+ */
+function renderPollChoices(choices) {
+  var $ul = $('#respond-to-poll-options');
+
+  choices.forEach(function (choice, index) {
+    $ul.append(renderChoiceElement(choice, index));
+  });
+
   $('#respond-to-poll-options').sortable({
     helper: 'clone',
     sort: function (e, ui) {
@@ -27,32 +139,23 @@ $(() => {
       });
     }
   });
+}
 
-  $.get('/polls/' + search)
-    .done(function (result) {
-      console.log(result);
-      result.choices.forEach(function (choice, index) {
-        $ul.append(createInput(choice, index));
-      });
-      $('.respond-to-poll-title').text(result.title);
-    });
+/**
+ * Return a poll choice <li> element
+ * @param {object} choice 
+ * @param {integer} index 
+ */
+function renderChoiceElement(choice, index) {
+  var $li;
+  if (choice.description.length === 0) {
+    $li = $(`<li class="list-group-item"><div class="sortable-number">${index + 1}</div><span class="choice-title">${choice.title}</span> <span class="choice-desc"></span></li>`);
+  } else {
+    $li = $(`<li class="list-group-item"><div class="sortable-number">${index + 1}</div><span class="choice-title">${choice.title}</span> <span class="choice-desc">(${choice.description})</span></li>`);
+  }
 
-  $('button').on('click', function (e) {
-    e.preventDefault();
-    let choiceArr = [];
-    let rank = $ul.children().length;
-    let poll_id;
-    $ul.children().each(function () {
-      let choiceObj = {};
-      poll_id = $(this).attr('poll_id');
-      choiceObj['choice_id'] = $(this).attr('id');
-      choiceObj['rank'] = rank;
-      choiceArr.push(choiceObj);
-      rank--;
-    });
-    $.post('/polls/' + search, { 'name': $('input').val(), 'poll_id': poll_id, 'choiceArr': choiceArr })
-      .done(function (res) {
+  $li.attr('id', choice.id);
+  $li.attr('poll_id', choice.poll_id);
 
-      });
-  });
-});
+  return $li;
+}
