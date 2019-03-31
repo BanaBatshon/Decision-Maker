88d148a4c8336a969d788d239b6cc8f448bb74df
Bana <banabatshon@hotmail.com>
2019-03-31 17:02:21 +0000

    changes the seeded data to prevent the ranks from containing a 0 or be repeated more than once per submission

--- db/seeds/03_submissions_choices.js
+++ db/seeds/03_submissions_choices.js
@@ -11,35 +11,35 @@ exports.seed = function(knex, Promise) {
     console.log(rows);
     const [submission_id, choice_id] = rows;
 
-    const submission_choices = [];
-    submission_id.forEach(submission => {
-      choice_id.forEach(choice => {
-        submission_choices.push({
-          submission_id: submission.id,
-          choice_id: choice.id,
-          rank: Math.floor(Math.random() * choice_id.length)
-        });
-      });
-    });
+    // const submission_choices = [];
+    // submission_id.forEach(submission => {
+    //   choice_id.forEach(choice => {
+    //     submission_choices.push({
+    //       submission_id: submission.id,
+    //       choice_id: choice.id,
+    //       rank: Math.floor(Math.random() * (choice_id.length) + 1)
+    //     });
+    //   });
+    // });
 
-    console.log(submission_choices);
+    // console.log(submission_choices);
 
-    return knex('submission_choices').insert(submission_choices);
+    // return knex('submission_choices').insert(submission_choices);
 
 
-    // return Promise.all([
-    //   knex('submission_choices').insert({submission_id: rows[0][0].id, choice_id: rows[1][0].id, rank:0}),
-    //   knex('submission_choices').insert({submission_id: rows[0][0].id, choice_id: rows[1][1].id, rank:1}),
-    //   knex('submission_choices').insert({submission_id: rows[0][0].id, choice_id: rows[1][2].id, rank:2}),
-    //   knex('submission_choices').insert({submission_id: rows[0][0].id, choice_id: rows[1][3].id, rank:3}),
-    //   knex('submission_choices').insert({submission_id: rows[0][1].id, choice_id: rows[1][0].id, rank:0}),
-    //   knex('submission_choices').insert({submission_id: rows[0][1].id, choice_id: rows[1][1].id, rank:1}),
-    //   knex('submission_choices').insert({submission_id: rows[0][1].id, choice_id: rows[1][2].id, rank:2}),
-    //   knex('submission_choices').insert({submission_id: rows[0][1].id, choice_id: rows[1][3].id, rank:3}),
-    //   knex('submission_choices').insert({submission_id: rows[0][2].id, choice_id: rows[1][0].id, rank:0}),
-    //   knex('submission_choices').insert({submission_id: rows[0][2].id, choice_id: rows[1][1].id, rank:1}),
-    //   knex('submission_choices').insert({submission_id: rows[0][2].id, choice_id: rows[1][2].id, rank:2}),
-    //   knex('submission_choices').insert({submission_id: rows[0][2].id, choice_id: rows[1][3].id, rank:3})
-    // ])
+    return Promise.all([
+      knex('submission_choices').insert({submission_id: rows[0][0].id, choice_id: rows[1][0].id, rank:1}),
+      knex('submission_choices').insert({submission_id: rows[0][0].id, choice_id: rows[1][1].id, rank:2}),
+      knex('submission_choices').insert({submission_id: rows[0][0].id, choice_id: rows[1][2].id, rank:3}),
+      // knex('submission_choices').insert({submission_id: rows[0][0].id, choice_id: rows[1][3].id, rank:4}),
+      knex('submission_choices').insert({submission_id: rows[0][1].id, choice_id: rows[1][0].id, rank:2}),
+      knex('submission_choices').insert({submission_id: rows[0][1].id, choice_id: rows[1][1].id, rank:1}),
+      knex('submission_choices').insert({submission_id: rows[0][1].id, choice_id: rows[1][2].id, rank:3}),
+      // knex('submission_choices').insert({submission_id: rows[0][1].id, choice_id: rows[1][3].id, rank:1}),
+      knex('submission_choices').insert({submission_id: rows[0][2].id, choice_id: rows[1][0].id, rank:1}),
+      knex('submission_choices').insert({submission_id: rows[0][2].id, choice_id: rows[1][1].id, rank:3}),
+      knex('submission_choices').insert({submission_id: rows[0][2].id, choice_id: rows[1][2].id, rank:2}),
+      // knex('submission_choices').insert({submission_id: rows[0][2].id, choice_id: rows[1][3].id, rank:4})
+    ])
   })
 }
